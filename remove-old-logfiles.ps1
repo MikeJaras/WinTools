ab30de0649daf528eb7a8df477406944f4a8e1d3
MikeJaras <33084283+MikeJaras@users.noreply.github.com>
2018-02-26 16:07:27 +0100

    Extended func and adoptation to many files.

--- remove-old-logfiles.ps1
+++ remove-old-logfiles.ps1
@@ -1,8 +1,10 @@
-﻿$RetainedDays = 12
-#$ProcessFiles = New-Object System.Collections.ArrayList
-#$ProcessFiles = @()
+$RetainedDays = 112
 $RootPath = "c:\temp\"
+$ArchivePath = "c:\temp\arkiv"
 $ArchiveBoundary = (Get-Date).AddDays(-$RetainedDays).ToString('yyyy-MM-dd')
+$ProcessFiles = New-Object System.Collections.ArrayList
+#$ProcessFiles = @()
+# Om man vill filtrera ytterligare, tex om man har andra filer i samma katalog.
 #$Candidates = (Get-ChildItem -Path $RootPath).FullName |
 #    Where-Object { ($_.BaseName -split '_|\.')[1] -lt $ArchiveBoundary }
 
@@ -10,14 +12,41 @@ $Today = get-date -format yyyy-MM-dd
 $Today = [datetime]::ParseExact($Today,"yyyy-MM-dd", $null)
 #$Today = get-date -format (Get-Culture).DateTimeFormat.ShortDatePattern
 $FileNameRegex  = "([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))"
-foreach ($File in (Get-ChildItem -Path $RootPath).FullName) {
+
+$targets = (Get-ChildItem -Path $RootPath)
+foreach ($File in $targets) {
+#foreach ($File in (Get-ChildItem -Path $RootPath).FullName) {
 
    $match = [regex]::matches($File, $FileNameRegex )
    if ($match -ne "") {
     $FileNameDate = $match[0].Groups[1].Value
+#    $File
 #    ($Today - [datetime]$FileNameDate).days #skriver hur gammal filen är i dagar.
     if (($Today - [datetime]$FileNameDate).days  -ge $RetainedDays) { $ProcessFiles += $File }
    }
 }
 
-$ProcessFiles
+if ($ProcessFiles) {
+    Write-Host "Listing target files to archive"
+    $ProcessFiles
+}
+else {
+    Write-Host "No files to move"
+    { exit }
+}
+
+write-host -nonewline "Continue moving the files to Archive? (Y/N) "
+$response = read-host
+if ( $response -ne "Y" ) { exit }
+
+foreach ($File in $ProcessFiles) {
+    $FileFullName = join-path -path $RootPath -childpath $File
+    move-item $FileFullName -Destination $ArchivePath
+    }
+    
+Write-Host "Done moving files"
+write-host -nonewline "List all files in archive? (Y/N) "
+$response = read-host
+if ( $response -ne "Y" ) { exit }
+
+Get-ChildItem -Path $ArchivePath
